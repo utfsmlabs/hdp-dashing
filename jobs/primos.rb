df00f6dda0509e3ac8129905d1a02b7a1bf219e2
bdfantini <bdfantini@gmail.com>
2013-08-14 10:41:49 -0400

    Inicial

---
+++ jobs/primos.rb
@@ -0,0 +1,5 @@
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '10s', :first_in => 0 do |job|
+	
+	send_event('primos', { current: rand(1000) })
+end
