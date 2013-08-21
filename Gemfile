affcc0e805d5a1312693bf8864924851cb29950c
bdfantini <bdfantini@gmail.com>
2013-08-21 14:15:56 -0400

    listo labs

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 gem 'dashing'
 
 ## Remove this if you don't need a twitter widget.
+gem 'therubyracer'
 gem 'twitter'
--- jobs/labs.rb
+++ jobs/labs.rb
@@ -1,14 +1,13 @@
+require 'open-uri'
 settings = YAML.load_file("config.yaml")
-hash_tareas = Hash.new
+pcs = Hash.new
 
 SCHEDULER.every '1m', :first_in => 0 do |job|	
-  base_url = URI("http//meru/icinga/cgi-bin/status.cgi?hostgroup=lds&hostgroup=lpa&style=overview&scroll=0&hoststatustypes=4&jsonoutput")
-  req = Net::HTTP.get(base_url)
-  req.basic_auth 'Settings.','Settings.'
-  res = Net::HTTP.start(base_url.hostname, base_url.port){|http|
-    http.request(req)
-  }
-  pcs_apagados = JSON.parse(res.body).force_encoding('utf-8'))["status"]["hostgroup_overview"].flat_map{|group| group["members"]}.map{|host| host["host_name"]}
-
-  send_event('icingalabs_list', { pcs: pcs_apagados })
+  url = "http://meru/icinga/cgi-bin/status.cgi?hostgroup=lds&hostgroup=lpa&style=overview&scroll=0&hoststatustypes=4&jsonoutput"
+  @labs = JSON.parse(open(url,:http_basic_authentication=>['icingaadmin',settings["config"]["icinga"]]).read)["status"]["hostgroup_overview"].flat_map{|group| group["members"]}.map{|host| host["host_name"]}
+  @labs.each do |pc|
+		pcs[pc] = { label: pc }
+	end
+  pcss = pcs.sample 5
+  send_event('labs', { items: pcss.values })
 end
