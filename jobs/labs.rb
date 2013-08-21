f0b36aeeaac163b1e2aa6e93697ef850d1c12020
bdfantini <bdfantini@gmail.com>
2013-08-21 16:32:48 -0400

    Varias cosas, vean el có¡go, ue paja explicarlo :)

--- jobs/labs.rb
+++ jobs/labs.rb
@@ -2,12 +2,11 @@ require 'open-uri'
 settings = YAML.load_file("config.yaml")
 pcs = Hash.new
 
-SCHEDULER.every '1m', :first_in => 0 do |job|	
+SCHEDULER.every '5s', :first_in => 0 do |job|	
   url = "http://meru/icinga/cgi-bin/status.cgi?hostgroup=lds&hostgroup=lpa&style=overview&scroll=0&hoststatustypes=4&jsonoutput"
-  @labs = JSON.parse(open(url,:http_basic_authentication=>['icingaadmin',settings["config"]["icinga"]]).read)["status"]["hostgroup_overview"].flat_map{|group| group["members"]}.map{|host| host["host_name"]}
-  @labs.each do |pc|
-		pcs[pc] = { label: pc }
-	end
-  pcss = pcs.sample 5
-  send_event('labs', { items: pcss.values })
+  labs = JSON.parse(open(url,:http_basic_authentication=>['icingaadmin',settings["config"]["icinga"]]).read)["status"]["hostgroup_overview"].flat_map{|group| group["members"]}
+  labs.map! do |host| {name: host["host_name"]}
+  end
+  send_event('labs', { labs: labs })
 end
+
