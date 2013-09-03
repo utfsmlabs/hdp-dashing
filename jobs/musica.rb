d83f4629f6bc263fa4a70620816c6aec59bbf00d
bdfantini <bdfantini@gmail.com>
2013-09-03 13:28:00 -0400

    pico

--- jobs/musica.rb
+++ jobs/musica.rb
@@ -1,10 +1,13 @@
 require 'open-uri'
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '5s', :first_in => 0 do |job|  	
-	url = "http://localhost:9000/player/command.php?currentsong"
+	url = "http://bender:9000/player/command.php?currentsong"
 	response = JSON.parse (open(url).read.gsub(/[()]/,""))
-	#puts response
+	
 	cancion = response['result']['Artist'].to_s + " - " + response['result']['Title'].to_s
-	#puts cancion
-	send_event('musica', { text: cancion })
+	if cancion != " - "
+		send_event('musica', { text: cancion })
+	else
+		send_event('musica', { text: "Pon musica fome culiao" })
+	end
 end
