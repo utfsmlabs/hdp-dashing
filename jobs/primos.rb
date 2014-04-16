25c2f93970087a19cc3065dbf119393023124622
Nicolas Aravena <nicolas@aravenas.com>
2014-04-16 18:00:09 -0300

    arreglando parseo de primos

--- jobs/primos.rb
+++ jobs/primos.rb
@@ -3,18 +3,24 @@ require 'net/http'
 SCHEDULER.every '1m', :first_in => 0 do |job|
 	primos = Hash.new
 	html = Net::HTTP.get(URI.parse('http://ctm.inf.santiago.usm.cl/pdts')).force_encoding('utf-8')
-	primos_arreglo = html.split(',')
+	primos_hash = ""
+	primos_hash = eval("primos_hash ="+html)
 
-	primos_arreglo.each do |primo|
-		if primo == "Fuera del horario de turnos"
-			primos[primo] = {label: "Fuera del"}
-			primos[primo] = {label: "Horario"}
-			primos[primo] = {label: "de Turnos"}
 
-		else
-			primos[primo] = { label: primo }
-		end
+	primos_hash.each do |primo, log|
+		primos[primo] = { label: primo }
 	end
+	
+	# primos_arreglo.each do |primo|
+	# 	if primo == "Fuera del horario de turnos"
+	# 		primos[primo] = {label: "Fuera del"}
+	# 		primos[primo] = {label: "Horario"}
+	# 		primos[primo] = {label: "de Turnos"}
+
+	# 	else
+	# 		primos[primo] = { label: primo }
+	# 	end
+	# end
 
     send_event('primos', { primos: primos.values })
 end
