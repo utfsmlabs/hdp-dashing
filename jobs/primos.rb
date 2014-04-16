216598897b32890c0f3a68d1e2a4846fcc52feb2
Nicolas Aravena <nicolas@aravenas.com>
2014-04-16 18:13:06 -0300

    Agregado Status del logueo para primos de turno

--- jobs/primos.rb
+++ jobs/primos.rb
@@ -4,11 +4,21 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
 	primos = Hash.new
 	html = Net::HTTP.get(URI.parse('http://ctm.inf.santiago.usm.cl/pdts')).force_encoding('utf-8')
 	primos_hash = ""
-	primos_hash = eval("primos_hash ="+html)
+	eval("primos_hash ="+html)
 
+	if primos_hash.empty?
+		primos[primo] = {label: "Fuera del"}
+		primos[primo] = {label: "Horario"}
+		primos[primo] = {label: "de Turnos"}
+	end
 
 	primos_hash.each do |primo, log|
 		primos[primo] = { label: primo }
+		if log
+			primos[primo] = { label: primo , icon: "✔" }
+		else
+			primos[primo] = { label: primo , icon: "✘" }
+		end
 	end
 	
 	# primos_arreglo.each do |primo|
