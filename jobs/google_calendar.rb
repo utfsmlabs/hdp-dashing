ec427e381a8c7e285396c5b8710b151046b6f4ba
Nicolás Aravena <mhobbit@gmail.com>
2016-03-30 13:18:09 -0300

    arreglacioness

---
+++ jobs/google_calendar.rb
@@ -0,0 +1,27 @@
+require 'icalendar'
+
+ical_url = 'https://calendar.google.com/calendar/ical/primos.stgo%40gmail.com/private-c9b51ef374fb7fb37deebe810e97d069/basic.ics'
+uri = URI ical_url
+
+Encoding.default_external = Encoding::UTF_8
+
+SCHEDULER.every '15s', :first_in => 4 do |job|
+  result = Net::HTTP.get uri
+
+  calendars = Icalendar.parse(result)
+  calendar = calendars.first
+
+  events = calendar.events.map do |event|
+    {
+      start: event.dtstart,
+      end: event.dtend,
+      summary: event.summary
+    }
+  end.select { |event| event[:start] > DateTime.now }
+
+  events = events.sort { |a, b| a[:start] <=> b[:start] }
+
+  events = events[0..5]
+
+  send_event('google_calendar', { events: events })
+end
